1dc56c8ddce9378b922107d0409ce06904733fa1
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-06 11:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
 
+const fname=	document.getElementById("fname");
+const lname= document.getElementById("lname"); 
+let name=fname.value+" "+lname.value 
+alert(name);
+
+	
+
 }
+
+const form =document.getElementById("form1");
+form.addEventListener("submit",getFormvalue)
